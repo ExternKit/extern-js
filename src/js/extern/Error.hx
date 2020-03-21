ade980aec8d72c966d2d05f6ea998ef801b66608
Kevin Leung <kevinresol@gmail.com>
2020-03-21 20:40:18 +0800

    Fix deprecation

--- src/js/extern/Error.hx
+++ src/js/extern/Error.hx
@@ -1,7 +1,9 @@
 package js.extern;
 
+import #if haxe4 js.lib.Error #else js.Error #end as JsError;
+
 @:forward
-abstract Error(Dynamic) from js.Error to js.Error from String to String
+abstract Error(Dynamic) from JsError to JsError from String to String
 #if js_kit
 from js.support.Error to js.support.Error 
 #end
